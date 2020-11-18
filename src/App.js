2d0bdebdfc3143ccffd931c217bd4575c5e5f7c6
Johan Garzon <johanprograma@hotmail.com>
2020-11-18 10:31:23 +0100

    Login page: simple login returning token.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'; // Delete ?
 import { Switch, Route } from 'react-router-dom'
 
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
 import PostListPage from './pages/PostListPage'
 import PostDetailPage from './pages/PostDetailPage'
 import PostCreatePage from './pages/PostCreatePage'
-import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
-import HomePage from './pages/HomePage'
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
         <Route path='/posts/create'> <PostCreatePage /> </Route>
         <Route path='/posts/:id'> <PostDetailPage /> </Route> {/* Make to dynamic routing */}
         <Route path='/posts'> <PostListPage /> </Route>
+        <Route path='/home'> <HomePage /> </Route>
         <Route path='/login'> <LoginPage /> </Route>
         <Route path='/register'> <RegisterPage /> </Route>
-        <Route path='/home'> <HomePage /> </Route>
 
         <Route path='/'> <div>Intro page</div> </Route>
       </Switch>
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [tempData, setTempData] = useState(null)
+
+  function handleEmailOnChange(e) {
+    setEmail(e.target.value)
+  }
+  function handlePasswordOnChange(e) {
+    setPassword(e.target.value)
+  }
+  function handleSubmit(event) {
+    console.log('Entrando')
+    event.preventDefault()
+    const options = {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ email, password })
+    }
+    console.log('options', options)
+    fetch('https://lab.willandskill.eu/api/v1/auth/api-token-auth/', options)
+    .then( response => response.json())
+    .then( data => {
+      console.log('HOlaaa')
+      console.log('data', data)
+    })
+  }
+
   return (
     <div>
-      <h1>Login page</h1>
+      <form>
+        <input type="text" onChange={handleEmailOnChange} name="email" placeholder="E-mail" />
+        <input type="password" onChange={handlePasswordOnChange} name="password" placeholder="Password" />
+        <input type="submit" onClick={handleSubmit} value="Login" />
+      </form>
+      <div>
+        <p>{email}</p>
+        <p>{password}</p>
+        <p>{tempData}</p>
+      </div>
     </div>
   )
 }
